753bb0f258f753985c7d38be69c6f119867eb8fd
invictus1902 <131914647+invictus1902@users.noreply.github.com>
2024-11-20 16:44:48 +0600

    'emir20,11'

---
+++ src/about-us/about-us.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './about.scss'
+
+const About = () => {
+    return (
+        <div>
+            <h1>O nas</h1>
+        </div>
+    );
+};
+
+export default About;
